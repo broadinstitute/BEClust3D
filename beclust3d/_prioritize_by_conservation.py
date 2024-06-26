12e01c209bbfa64fb0af04db637f94caa4ca883b
Calvin XiaoYang Hu <calvinxyh@Calvins-MacBook-Pro.local>
2024-06-25 22:27:49 -0400

    translate Notebook 3.2.5 to _randomize_by_conservation.py and minor edits to _prioritize_by_conservation.py

--- beclust3d/_prioritize_by_conservation.py
+++ beclust3d/_prioritize_by_conservation.py
@@ -12,9 +12,9 @@ import math
 
 
 def prioritize_by_conservation(df_struc, df_consrv, 
-                               workdir, input_gene, structureid, 
-                               screenid, 
-                               ): 
+        workdir, input_gene, structureid, 
+        screenid, 
+): 
 
     df_struc_consvr = df_struc
     df_struc_consvr['human_res_pos'] = df_consrv['human_res_pos']
@@ -43,32 +43,36 @@ def prioritize_by_conservation(df_struc, df_consrv,
             
             if not math.isnan(human_res_pos): ### 
 
-                df_this_humanpos_edits = df_edit.loc[df_edit['human_pos'] == int(human_res_pos), ]
-                df_this_humanpos_edits = df_this_humanpos_edits.reset_index()
+                df_pos_edits = df_edit.loc[df_edit['human_pos'] == int(human_res_pos), ]
+                df_pos_edits = df_pos_edits.reset_index()
 
-                if len(df_this_humanpos_edits) > 1:
-                    #edit
-                    this_humanpos_edits_list = df_this_humanpos_edits['edit'].tolist()
-                    this_humanpos_edits_str = ';'.join(list(set(this_humanpos_edits_list)))
-                    #score
-                    this_humanpos_LFCscore_list = df_this_humanpos_edits['LFC'].tolist()
-                    this_humanpos_LFCscore = round(sum(this_humanpos_LFCscore_list) / len(this_humanpos_LFCscore_list), 3)
-                    this_humanpos_LFCscore_str = str(this_humanpos_LFCscore)
+                if len(df_pos_edits) > 1:
+                    # edit
+                    pos_edits_list = df_pos_edits['edit'].tolist()
+                    pos_edits_str = ';'.join(list(set(pos_edits_list)))
+                    # score
+                    pos_LFCscore_list = df_pos_edits['LFC'].tolist()
+                    pos_LFCscore = round(sum(pos_LFCscore_list) / len(pos_LFCscore_list), 3)
+                    pos_LFCscore_str = str(pos_LFCscore)
 
-                elif len(df_this_humanpos_edits) == 1:   
-                    this_humanpos_edits_str = df_this_humanpos_edits.at[0,'edit']
-                    this_humanpos_LFCscore_str = str(round(df_this_humanpos_edits.at[0,'LFC'], 3))
+                elif len(df_pos_edits) == 1:   
+                    pos_edits_str = df_pos_edits.at[0,'edit']
+                    pos_LFCscore_str = str(round(df_pos_edits.at[0,'LFC'], 3))
                 else:
-                    this_humanpos_edits_str = '-'
-                    this_humanpos_LFCscore_str = '-'
+                    pos_edits_str = '-'
+                    pos_LFCscore_str = '-'
 
-                unique_LFC_per_residue_human = this_humanpos_LFCscore_str
-                all_edits_per_residue_human = this_humanpos_edits_str
+                unique_LFC_per_residue_human = pos_LFCscore_str
+                all_edits_per_residue_human = pos_edits_str
 
             arr_unique_LFC.append(unique_LFC_per_residue_human)
             arr_all_edits.append(all_edits_per_residue_human)
 
         df_struc_consvr[f'all_{edit_type}_edits'] = arr_all_edits
         df_struc_consvr[f'mean_{edit_type}_LFC'] = arr_unique_LFC
+
+    edits_filedir = Path(workdir + input_gene)
+    out_filename = edits_filedir / f"screendata/{input_gene}_{structureid}_struc_consrv_proteinedits.tsv"
+    df_struc_consvr.to_csv(out_filename, sep = '\t', index=False)
 
     return df_struc_consvr
