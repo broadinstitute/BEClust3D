e5bdb0ca4242d84159f056618d548f5421da88b0
Calvin XiaoYang Hu <calvinxyh@dhcp-10-250-207-248.harvard.edu>
2024-07-25 23:50:16 -0400

    meta aggregation tests

--- beclust3d/metaaggregation.py
+++ beclust3d/metaaggregation.py
@@ -16,14 +16,13 @@ from pathlib import Path
 from scipy.stats import mannwhitneyu
 import os
 from binning_lfc3d import binning
-# from warnings import simplefilter
-# simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
+import warnings
 
 def metaaggregation(
     df_LFC_LFC3D,
     workdir, 
-    input_gene, input_uniprot, structureid, 
-    input_screen, 
+    input_gene, structureid, 
+    input_screens, 
     nRandom=1000, 
 ): 
     """
@@ -39,8 +38,6 @@ def metaaggregation(
             the working directory
         input_gene: str, required
             the name of the input human gene
-        input_uniprot: str, required
-            the Uniprot ID for a particular protein
         input_screen: str, required
             the name of the input screen
         structureid: str, required
@@ -55,87 +52,92 @@ def metaaggregation(
     
     # Sum LFC3D #
 
-    screen_name = input_screen.split('.')[0]
     df_METAggregation = pd.DataFrame()
-
     df_METAggregation['unipos'] = df_LFC_LFC3D['unipos']
     df_METAggregation['unires'] = df_LFC_LFC3D['unires']
     
-    LFC3Ds = []
     list_sum_LFC3D_neg, list_sum_LFC3D_pos = [], []
     for i in range(0, len(df_LFC_LFC3D)):
-        this_res_sum_LFC3D_neg, this_res_sum_LFC3D_pos = 0.0, 0.0
-        header_LFC3D = f"{screen_name}_LFC3D"
+        res_sum_LFC3D_neg, res_sum_LFC3D_pos = 0.0, 0.0
+
+        for input_screen in input_screens: 
+            screen_name = input_screen.split('.')[0]
+            header_LFC3D = f"{screen_name}_LFC3D"
+            if header_LFC3D not in df_LFC_LFC3D.columns: 
+                warnings.warn(f'{header_LFC3D} not in input df_LFC_LFC3D')
+                continue
+            
+            LFC3D = df_LFC_LFC3D.at[i, header_LFC3D]
+            if (LFC3D != '-') and (float(LFC3D) < 0.0):
+                res_sum_LFC3D_neg += float(LFC3D)
+            if (LFC3D != '-') and (float(LFC3D) > 0.0):
+                res_sum_LFC3D_pos += float(LFC3D)
         
-        LFC3D = df_LFC_LFC3D.at[i, header_LFC3D]
-        LFC3Ds.append(LFC3D)
-        if (LFC3D != '-') and (float(LFC3D) < 0.0):
-            this_res_sum_LFC3D_neg += float(LFC3D)
-        if (LFC3D != '-') and (float(LFC3D) > 0.0):
-            this_res_sum_LFC3D_pos += float(LFC3D)
-        
-        list_sum_LFC3D_neg.append(this_res_sum_LFC3D_neg)
-        list_sum_LFC3D_pos.append(this_res_sum_LFC3D_pos)
+        list_sum_LFC3D_neg.append(res_sum_LFC3D_neg)
+        list_sum_LFC3D_pos.append(res_sum_LFC3D_pos)
 
     df_METAggregation['SUM_LFC3D_neg'] = list_sum_LFC3D_neg 
     df_METAggregation['SUM_LFC3D_pos'] = list_sum_LFC3D_pos
+    del list_sum_LFC3D_neg, list_sum_LFC3D_pos
 
     dict_META = {}
     for r in range(0, nRandom):
         list_sum_LFC3D_neg, list_sum_LFC3D_pos = [], []
-        for i in range(0, len(df_LFC_LFC3D)):
-            this_res_sum_LFC3D_neg, this_res_sum_LFC3D_pos = 0.0, 0.0
-            header_LFC3D = f"{screen_name}_LFC3Dr{str(r+1)}"
 
-            LFC3D = LFC3Ds[i]
-            if (LFC3D != '-') and (float(LFC3D) < 0.0):
-                this_res_sum_LFC3D_neg = this_res_sum_LFC3D_neg + float(LFC3D)
-            if (LFC3D != '-') and (float(LFC3D) > 0.0):
-                this_res_sum_LFC3D_pos = this_res_sum_LFC3D_pos + float(LFC3D)
+        for i in range(0, len(df_LFC_LFC3D)): 
+            res_sum_LFC3D_neg, res_sum_LFC3D_pos = 0.0, 0.0
 
-            list_sum_LFC3D_neg.append(this_res_sum_LFC3D_neg)
-            list_sum_LFC3D_pos.append(this_res_sum_LFC3D_pos)
+            for input_screen in input_screens: # sum across screens
+                screen_name = input_screen.split('.')[0]
+                header_LFC3D = f"{screen_name}_LFC3Dr{str(r+1)}"
+                if header_LFC3D not in df_LFC_LFC3D.columns: 
+                    # warnings.warn(f'{header_LFC3D} not in input df_LFC_LFC3D')
+                    continue
+
+                LFC3D = df_LFC_LFC3D.at[i, header_LFC3D]
+                if (LFC3D != '-') and (float(LFC3D) < 0.0):
+                    res_sum_LFC3D_neg += float(LFC3D)
+                if (LFC3D != '-') and (float(LFC3D) > 0.0):
+                    res_sum_LFC3D_pos += float(LFC3D)
+
+            list_sum_LFC3D_neg.append(res_sum_LFC3D_neg)
+            list_sum_LFC3D_pos.append(res_sum_LFC3D_pos)
 
         dict_META[f'SUM_LFC3Dr{str(r+1)}_neg'] = list_sum_LFC3D_neg 
         dict_META[f'SUM_LFC3Dr{str(r+1)}_pos'] = list_sum_LFC3D_pos
+        del list_sum_LFC3D_neg, list_sum_LFC3D_pos
+
     df_METAggregation = pd.concat([df_METAggregation, pd.DataFrame(dict_META)], axis=1)
+    del dict_META
 
     # Average of SUM of randomized signal, as the background noise #
 
     list_avg_LFC3D_neg, list_avg_LFC3D_pos = [], []
-    for i in range(0, len(df_METAggregation)):
-        this_res_sum_LFC3D_neg, this_res_sum_LFC3D_pos = 0.0, 0.0
-        this_res_avg_LFC3D_neg, this_res_avg_LFC3D_pos = 0.0, 0.0
+    for i in range(0, len(df_METAggregation)): 
+        res_sum_LFC3D_neg, res_sum_LFC3D_pos = 0.0, 0.0
 
-        for r in range(0, nRandom):
-            LFC3D = df_METAggregation.at[i, f'SUM_LFC3Dr{str(r+1)}_neg']
-            this_res_sum_LFC3D_neg += float(LFC3D)
-            this_res_avg_LFC3D_neg = this_res_sum_LFC3D_neg / nRandom
-                
-            LFC3D = df_METAggregation.at[i, f'SUM_LFC3Dr{str(r+1)}_pos']
-            this_res_sum_LFC3D_pos += float(LFC3D)
-            this_res_avg_LFC3D_pos = this_res_sum_LFC3D_pos / nRandom
+        for r in range(0, nRandom): 
+            LFC3D_neg = df_METAggregation.at[i, f'SUM_LFC3Dr{str(r+1)}_neg']
+            res_sum_LFC3D_neg += float(LFC3D_neg)
+            LFC3D_pos = df_METAggregation.at[i, f'SUM_LFC3Dr{str(r+1)}_pos']
+            res_sum_LFC3D_pos += float(LFC3D_pos)
         
-        list_avg_LFC3D_neg.append(this_res_avg_LFC3D_neg)
-        list_avg_LFC3D_pos.append(this_res_avg_LFC3D_pos)
+        list_avg_LFC3D_neg.append(res_sum_LFC3D_neg / nRandom)
+        list_avg_LFC3D_pos.append(res_sum_LFC3D_pos / nRandom)
 
     df_METAggregation['AVG_LFC3Dr_neg'] = list_avg_LFC3D_neg 
     df_METAggregation['AVG_LFC3Dr_pos'] = list_avg_LFC3D_pos
+    del list_avg_LFC3D_neg, list_avg_LFC3D_pos
 
     # convert signal to Z score #
 
-    list_LFC3D_neg_zscore, list_LFC3D_neg_pval, list_LFC3D_neg_pval_label = [], [], []
-    list_LFC3D_pos_zscore, list_LFC3D_pos_pval, list_LFC3D_pos_pval_label = [], [], []
-    lists_LFC3D = [{'z':list_LFC3D_neg_zscore, 'p':list_LFC3D_neg_pval, 'lab':list_LFC3D_neg_pval_label}, 
-                   {'z':list_LFC3D_pos_zscore, 'p':list_LFC3D_pos_pval, 'lab':list_LFC3D_pos_pval_label}
-                   ]
-
-    mu_neg = df_METAggregation['AVG_LFC3Dr_neg'].mean()
-    sigma_neg = df_METAggregation['AVG_LFC3Dr_neg'].std()
-    mu_pos = df_METAggregation['AVG_LFC3Dr_pos'].mean()
-    sigma_pos = df_METAggregation['AVG_LFC3Dr_pos'].std()
-    params = [{'mu':mu_neg, 's':sigma_neg}, {'mu':mu_pos, 's':sigma_pos}]
-
+    lists_LFC3D = [{'z':[], 'p':[], 'lab':[]}, 
+                    {'z':[], 'p':[], 'lab':[]}, 
+                    ]
+    params = [{'mu':df_METAggregation['AVG_LFC3Dr_neg'].mean(), 
+                's':df_METAggregation['AVG_LFC3Dr_neg'].std()}, 
+                {'mu':df_METAggregation['AVG_LFC3Dr_pos'].mean(), 
+                's':df_METAggregation['AVG_LFC3Dr_pos'].std()}]
     colnames = ['SUM_LFC3D_neg', 'SUM_LFC3D_pos']
     pthr = 0.001
 
@@ -143,19 +145,21 @@ def metaaggregation(
         # sensitizing and resistant
         for colname, param, lists in zip(colnames, params, lists_LFC3D): 
             signal = float(df_METAggregation.at[i, colname])
-            signal_z = statistics.NormalDist(mu=param['mu'], 
-                                             sigma=param['s']).zscore(signal)
-            signal_p = stats.norm.sf(abs(signal_z))
+            if param['s'] == 0: 
+                lists['z'].append(0)
+                lists['p'].append(0)
+                lists['lab'].append(0)
+                continue
             
-            if signal_p < pthr: signal_plabel = f'p<{str(pthr)}'
-            else:               signal_plabel = f'p>={str(pthr)}'
-
+            signal_z = statistics.NormalDist(mu=param['mu'], sigma=param['s']).zscore(signal)
+            signal_p = stats.norm.sf(abs(signal_z))
+            signal_plabel = f'p<{str(pthr)}' if signal_p < pthr else f'p>={str(pthr)}'                
             lists['z'].append(signal_z)
             lists['p'].append(signal_p)
             lists['lab'].append(signal_plabel)
-        
-    dict_META = {'SUM_LFC3D_neg_z':list_LFC3D_neg_zscore, 'SUM_LFC3D_neg_p':list_LFC3D_neg_pval, 'SUM_LFC3D_neg_psig':list_LFC3D_neg_pval_label, 
-                 'SUM_LFC3D_pos_z':list_LFC3D_pos_zscore, 'SUM_LFC3D_pos_p':list_LFC3D_pos_pval, 'SUM_LFC3D_pos_psig':list_LFC3D_pos_pval_label, }
+    
+    dict_META = {'SUM_LFC3D_neg_z':lists_LFC3D[0]['z'], 'SUM_LFC3D_neg_p':lists_LFC3D[0]['p'], 'SUM_LFC3D_neg_psig':lists_LFC3D[0]['lab'], 
+                 'SUM_LFC3D_pos_z':lists_LFC3D[1]['z'], 'SUM_LFC3D_pos_p':lists_LFC3D[1]['p'], 'SUM_LFC3D_pos_psig':lists_LFC3D[1]['lab'], }
     df_METAggregation = pd.concat([df_METAggregation, pd.DataFrame(dict_META)], axis=1)
 
     edits_filedir = Path(workdir + '/' + input_gene)
@@ -163,20 +167,26 @@ def metaaggregation(
         os.mkdir(edits_filedir)
     if not os.path.exists(edits_filedir / 'metaaggregation'):
         os.mkdir(edits_filedir / 'metaaggregation')
+    if not os.path.exists(edits_filedir / 'plots'):
+        os.mkdir(edits_filedir / 'plots')
 
-    filename = edits_filedir / f"metaaggregation/{structureid}_{screen_name}_MetaAggr_LFC3D_and_randomized_background.tsv"
+    filename = edits_filedir / f"metaaggregation/{structureid}_MetaAggr_LFC3D_and_randomized_background.tsv"
     df_METAggregation.to_csv(filename, "\t", index=False)
 
-    # CALL PLOTS #
-    end_plots = [('SUM_LFC3D_neg', 'AVG_LFC3Dr_neg', 'sensitizing'), 
-                 ('SUM_LFC3D_pos', 'AVG_LFC3Dr_pos', 'resistant')]
-    for avg, sum, out in end_plots: 
-        res = metaaggregation_histogram(
-            df_METAggregation, 
-            edits_filedir, input_gene, screen_name, 
-            avg_colname=avg, sum_colname=sum, out_keyword=out, 
+    # HISTOGRAMS #
+    res_neg = metaaggregation_histogram(
+        df_METAggregation, edits_filedir, input_gene, 
+        avg_colname='AVG_LFC3Dr_neg', sum_colname='SUM_LFC3D_neg', out_keyword='sensitizing', 
+    )
+    res_pos = metaaggregation_histogram(
+        df_METAggregation, edits_filedir, input_gene, 
+        avg_colname='AVG_LFC3Dr_pos', sum_colname='SUM_LFC3D_pos', out_keyword='resistant', 
         )
-        print(res)
+    if res_neg is None or res_pos is None: 
+        return None
+    print(res_neg)
+    print(res_pos)
+        
     df_METAggregation = binning_lfc3d(df_METAggregation)
     # DISPLOTS #
     displots = [('SUM_LFC3D_neg_dis', 'SUM_LFC3D_neg', 'SUM_LFC3D_neg_psig', 'neg_pvalue'), 
@@ -186,7 +196,7 @@ def metaaggregation(
                 ]
     for filter, x, hue, name in displots: 
         metaaggregation_displot(
-            df_METAggregation, screen_name, 
+            df_METAggregation, 
             filter_col=filter, x_col=x, hue_col=hue, 
             filedir=edits_filedir, input_gene=input_gene, out_keyword=name
         )
@@ -195,7 +205,7 @@ def metaaggregation(
                     ('SUM_LFC3D_pos_dis', 'SUM_LFC3D_pos_psig', 'SUM_LFC3D_pos', 'pos_dis')]
     for dis, pval, y, out in scatterplots: 
         metaaggregation_scatterplot(
-                df_METAggregation, screen_name, 
+                df_METAggregation, 
                 dis_col=dis, pval_col=pval, y_col=y,
                 filedir=edits_filedir, input_gene=input_gene, out_keyword=out,
         )
@@ -204,8 +214,7 @@ def metaaggregation(
 
 
 def metaaggregation_histogram(
-        df_METAggregation, 
-        filedir, input_gene, screen_name, 
+        df_METAggregation, filedir, input_gene, 
         avg_colname, sum_colname, out_keyword, 
 ): 
     """
@@ -228,29 +237,33 @@ def metaaggregation_histogram(
                           df_METAggregation_plot[avg_colname])
     results['pearsonr r'], results['pearsonr p'] = r, p
 
+    # SUM #
     temp = df_METAggregation_plot[sum_colname]
-    mu, sigma = temp.mean(), temp.std()
-    results['sum min'], results['sum mean'] = temp.min(), mu
-    results['sum med'], results['sum std'] = temp.median(), sigma
-
-    z = statistics.NormalDist(mu=mu, sigma=sigma).zscore(-4.6)
-    p_cdf = stats.norm.cdf(z)
-    p_sf = stats.norm.sf(abs(z))
-    results['z'], results['p cdf'], results['p sf'] = z, p_cdf, p_sf
-
-    temp = df_METAggregation_plot[avg_colname]
     results['sum min'], results['sum mean'] = temp.min(), temp.mean()
     results['sum med'], results['sum std'] = temp.median(), temp.std()
 
+    if results['sum std'] == 0: 
+        return None
+    z = statistics.NormalDist(mu=results['sum mean'], sigma=results['sum std']).zscore(-4.6)
+    results['z'], results['p cdf'], results['p sf'] = z, stats.norm.cdf(z), stats.norm.sf(abs(z))
+
+    # AVG #
+    temp = df_METAggregation_plot[avg_colname]
+    results['avg min'], results['avg mean'] = temp.min(), temp.mean()
+    results['avg med'], results['avg std'] = temp.median(), temp.std()
+
+    # PLOT #
     df_METAggregation_plot.plot.area(x='unipos', alpha=0.55, stacked = False)
+    plt.axhline(y = results['sum mean'], color = 'r', linestyle = '-')
+    plt.axhline(y = results['sum mean']-results['sum std'], color = 'r', linestyle = '--')
+
     plt.legend(loc='lower left', borderaxespad=0)
     plt.xticks(np.arange(0,len(df_METAggregation), 100))
-    plt.axhline(y = mu, color = 'r', linestyle = '-')
-    plt.axhline(y = mu-sigma, color = 'r', linestyle = '--')
     plt.title(out_keyword)
 
-    out_filename = filedir / f"plots/{input_gene}_{screen_name}_signal_vs_background_{out_keyword}.png"
-    plt.savefig(out_filename, dpi = 300)
+    out_filename = filedir / f"plots/{input_gene}_signal_vs_background_{out_keyword}.png"
+    plt.savefig(out_filename, dpi = 500)
+    del df_METAggregation_plot
 
     return results
 
@@ -297,7 +310,6 @@ def binning_lfc3d(
 
 def metaaggregation_displot(
         df_METAggregation, 
-        screen_name, 
         filter_col, x_col, hue_col, 
         filedir, input_gene, out_keyword, 
 ): 
@@ -313,12 +325,11 @@ def metaaggregation_displot(
     sns.displot(df_combined_clean, x=x_col, hue=hue_col, bins=50, palette='tab10')
     plt.title(out_keyword)
 
-    out_name = filedir / f"plots/{input_gene}_{screen_name}_Aggr_LFC3D_{out_keyword}_histogram.png"
+    out_name = filedir / f"plots/{input_gene}_Aggr_LFC3D_{out_keyword}_histogram.png"
     plt.savefig(out_name, dpi=300) 
 
 def metaaggregation_scatterplot(
         df_METAggregation, 
-        screen_name, 
         dis_col, pval_col, y_col, 
         filedir, input_gene, out_keyword,
 ): 
@@ -338,5 +349,5 @@ def metaaggregation_scatterplot(
     plt.xticks(np.arange(0,len(df_METAggregation), 100))
     plt.title(out_keyword)
 
-    outname = filedir / f"plots/{input_gene}_{screen_name}_Aggr_LFC3D_{out_keyword}_dot_per_residue.png"
+    outname = filedir / f"plots/{input_gene}_Aggr_LFC3D_{out_keyword}_dot_per_residue.png"
     plt.savefig(outname, dpi=300)
--- tests/test_metaaggregation.py
+++ tests/test_metaaggregation.py
@@ -8,32 +8,30 @@ import warnings
 from variables import *
 from beclust3d.metaaggregation import metaaggregation
 
-
 @pytest.mark.parametrize(("gene", "uniprot", "structid", "mouse_gene"), zip(all_genes, all_uniprots, all_structureids, all_mouse_genes))
 @pytest.mark.parametrize("screen", all_human_screens)
-def test_afstructuralfeatures_human(gene, uniprot, structid, mouse_gene, screen): 
+def test_metaaggregation_human(gene, uniprot, structid, mouse_gene, screen): 
 
-    screen_name = screen.split('.')[0]
-    filename = f'{workdir}/{gene}/LFC3D/{gene}_{screen_name}_LFC_LFC3D_per_Random_LFC3Dr.tsv'
+    filename = f'{workdir}/{gene}/LFC3D/{gene}_LFC_LFC3D_per_Random_LFC3Dr.tsv' # from calculate_lfc3d
     if not os.path.exists(filename): 
         warnings.warn(f"{filename} does not exist")
         return True
     
     df_LFC_LFC3D = pd.read_csv(filename, sep = "\t")
-    metaaggregation(
+    res = metaaggregation(
             df_LFC_LFC3D=df_LFC_LFC3D, 
             workdir=workdir, 
             input_gene=gene, 
-            input_uniprot=uniprot, 
             structureid=structid, 
-            input_screen=screen,
+            input_screens=[screen],
             )
 
-    assert f'{structid}_{screen_name}_MetaAggr_LFC3D_and_randomized_background.tsv' in os.listdir(f'{workdir}/{gene}/metaaggregation')
-    plot_dir = os.listdir(f'{workdir}/{gene}/plots')
-    assert f'{gene}_{screen_name}_signal_vs_background_sensitizing.png' in plot_dir
-    assert f'{gene}_{screen_name}_signal_vs_background_resistant.png' in plot_dir
-    assert f'{gene}_{screen_name}_Aggr_LFC3D_neg_pvalue_histogram.png' in plot_dir
-    assert f'{gene}_{screen_name}_Aggr_LFC3D_pos_pvalue_histogram.png' in plot_dir
-    assert f'{gene}_{screen_name}_Aggr_LFC3D_neg_dis_dot_per_residue.png' in plot_dir
-    assert f'{gene}_{screen_name}_Aggr_LFC3D_pos_dis_dot_per_residue.png' in plot_dir
+    assert f'{structid}_MetaAggr_LFC3D_and_randomized_background.tsv' in os.listdir(f'{workdir}/{gene}/metaaggregation')
+    if res is not None: 
+        plot_dir = os.listdir(f'{workdir}/{gene}/plots')
+        assert f'{gene}_signal_vs_background_sensitizing.png' in plot_dir
+        assert f'{gene}_signal_vs_background_resistant.png' in plot_dir
+        assert f'{gene}_Aggr_LFC3D_neg_pvalue_histogram.png' in plot_dir
+        assert f'{gene}_Aggr_LFC3D_pos_pvalue_histogram.png' in plot_dir
+        assert f'{gene}_Aggr_LFC3D_neg_dis_dot_per_residue.png' in plot_dir
+        assert f'{gene}_Aggr_LFC3D_pos_dis_dot_per_residue.png' in plot_dir
