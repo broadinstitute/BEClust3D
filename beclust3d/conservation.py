a73fb6875620a6097cef1af0f441d9bcb5b40e58
Calvin XiaoYang Hu <calvinxyh@Calvins-MacBook-Pro.local>
2024-08-01 00:46:00 -0400

    Refactored conservation

--- beclust3d/conservation.py
+++ beclust3d/conservation.py
@@ -11,12 +11,21 @@ from pathlib import Path
 import requests
 import sys, os
 import time
+import warnings
+
+cons_dict = {
+    '*': ('conserved', 3),
+    ':': ('similar', 2),
+    '.': ('weakly_similar', 1),
+    ' ': ('not_conserved', -1),
+}
 
 def conservation(
         workdir, 
         input_human_gene, input_mouse_gene, 
         input_human_uniid, input_mouse_uniid, 
         email, title, 
+        wait_time=30, 
 ): 
     """
     Description
@@ -42,160 +51,147 @@ def conservation(
             a dataframe listing sequence conservation and their indices for human and mouse
             with column headers alignment_pos human_res_pos human_res mouse_res_pos mouse_res conservation
     """
-
-    # query .fasta protein sequences
     edits_filedir = Path(workdir + '/' + input_human_gene)
-    human_seq = query_protein_fasta(input_human_uniid, edits_filedir)
-    mouse_seq = query_protein_fasta(input_mouse_uniid, edits_filedir)
+    if not os.path.exists(edits_filedir):
+        os.mkdir(edits_filedir)
 
-    with open(edits_filedir / f"sequences.fasta", "w") as text_file:
+    # QUERY UNIPROT AND WRITE TO sequences.fasta #
+    request_filename_human, request_filename_mouse = f"{input_human_uniid}.fasta", f"{input_mouse_uniid}.fasta"
+    human_seq = query_protein_fasta(edits_filedir, request_filename_human)
+    mouse_seq = query_protein_fasta(edits_filedir, request_filename_mouse)
+    seqs_filename = f"sequences.fasta"
+    with open(edits_filedir / seqs_filename, "w") as text_file:
         text_file.write(human_seq)
         text_file.write(mouse_seq)
 
-    # alignment
-    muscle_id, alignment_file = alignment_muscle(edits_filedir, 
-                                                 email, title, 
-                                                 input_human_gene, input_mouse_gene, 
-                                                )
+    # MUSCLE ALIGNMENT #
+    align_filename = f"Human{input_human_gene}_Mouse{input_mouse_gene}.align"
+    muscle_id = alignment_muscle(edits_filedir, seqs_filename, align_filename, 
+                                 email, title, wait_time )
     
-    # use MUSCLE to align human and mouse sequences
-    iAlign = open(alignment_file, "r")
-    i_lines = iAlign.readlines()[3:]
-    iAlign.close()
-
-    # parse .align file #
-    ind = i_lines[0].rfind(' ')
-    cleaned_ilines = [s[ind+1:].strip('\n') for s in i_lines]
-    cleaned_ilines = [s for s in cleaned_ilines if len(s) > 0]
-    human_aligned_res = list(''.join([s for i, s in enumerate(cleaned_ilines) if i%3 == 0]))
-    mouse_aligned_res = list(''.join([s for i, s in enumerate(cleaned_ilines) if (i-1)%3 == 0]))
-    score             = list(''.join([s for i, s in enumerate(cleaned_ilines) if (i-2)%3 == 0]))
-    # construct align_conservation dataframe #
-    cons_dict = {
-        '*': ('conserved', 3),
-        ':': ('similar', 2),
-        '.': ('weakly_similar', 1),
-        ' ': ('not_conserved', -1),
-    }
-    index  = [i+1 for i in range(len(human_aligned_res))]
-    dis, v = [cons_dict[s][0] for s in score], [cons_dict[s][1] for s in score]
-    colnames = ['alignment_pos', 'human_aligned_res', 'mouse_aligned_res', 'score', 'dis_score', 'v_score']
-    colvals  = [index, human_aligned_res, mouse_aligned_res, score, dis, v]
-    df_alignconserv = pd.DataFrame()
-    for name, col in zip(colnames, colvals): 
-        df_alignconserv[name] = col
-    # save align_conservation #
-    alignconserv_filename = edits_filedir / f"Human{input_human_gene}_Mouse{input_mouse_gene}_align_conservation.tsv"
-    df_alignconserv.to_csv(alignconserv_filename, sep='\t', index=False)
-
-    # residue map conservation #
-    i, j = 0, 0
-    human_res_pos, mouse_res_pos = [], []
-    for s in human_aligned_res: 
-        if s != '-': i += 1
-        human_res_pos.append(i)
-    for s in mouse_aligned_res: 
-        if s != '-': j += 1
-        mouse_res_pos.append(j)
-    # construct residuemap_conservation dataframe #
-    df_residuemap = pd.DataFrame()
-    colnames = ['alignment_pos', 'human_res_pos', 'human_res', 'mouse_res_pos', 'mouse_res', 'conservation']
-    colvals  = [index, human_res_pos, human_aligned_res, mouse_res_pos, mouse_aligned_res, dis]
-    for name, col in zip(colnames, colvals): 
-        df_residuemap[name] = col
-    df_residuemap = df_residuemap[df_residuemap['human_res'] != '-']
-    # save residuemap_conservation #
-    residuemap_list_filename = edits_filedir / f"Human{input_human_gene}_Mouse{input_mouse_gene}_residuemap_conservation.tsv"
-    df_residuemap.to_csv(residuemap_list_filename, sep='\t', index=False)
+    # PARSE ALIGNMENT #
+    alignconserv_filename = f"Human{input_human_gene}_Mouse{input_mouse_gene}_align_conservation.tsv"
+    residuemap_filename =  f"Human{input_human_gene}_Mouse{input_mouse_gene}_residuemap_conservation.tsv"
+    df_alignconserv, df_residuemap = parse_alignment(edits_filedir, align_filename, 
+                                                     alignconserv_filename, residuemap_filename)
 
     return df_alignconserv, df_residuemap
 
 def query_protein_fasta(
-        input_uniprot, edits_filedir
+    edits_filedir, request_filename
 ): 
     """
     Description
         Query a Uniprot ID for .fasta file
-
     Params
-        input_uniprot: str, required
-            The Uniprot ID for a particular protein
-
+        edits_filedir: str, required
+            the Path to the main directory
     Returns
-        response_str: str
-            The amino acid sequence corresponding to the Uniprot ID
+        response_body: str
+            The amino acid sequence of the Uniprot ID
     """
 
+    # QUERY UNIPROT #
     url = "https://rest.uniprot.org/uniprotkb/"
-    request = f"{input_uniprot}.fasta"
-    requestURL = url+request
+    requestURL = url+request_filename
     response = requests.get(requestURL)
-
     if not response.ok:
         response.raise_for_status()
         sys.exit()
 
+    # RESPONSE TEXT #
     response_body = response.text
-    response_list = response_body.split('\n')
-    response_str = ''.join(response_list[1:])
-
-    with open(edits_filedir / request, "w") as text_file:
+    with open(edits_filedir / request_filename, "w") as text_file:
         text_file.write(response_body)
     return response_body
 
-# from Bio.Align.Applications import MuscleCommandline
-# from Bio import AlignIO
-
 def alignment_muscle(
-        edits_filedir, 
-        email, title, 
-        input_human_gene, input_mouse_gene, 
+    edits_filedir, 
+    seqs_filename, align_filename, 
+    email, title, wait_time, 
 ): 
     """
     Description
         Query ebi.ac.uk for MUSCLE alignment
-
     Params
         edits_filedir: str, required
             the Path to the main directory
         email: str, required
             email
-        email: str, required
+        title: str, required
             title for your queried job
-        input_human_gene: str, required
-            the name of the input human gene
-        input_mouse_gene: str, required
-            the name of the input mouse gene
-
     Returns
         job_url_code: str
             a code for the MUSCLE job
-        outpath_name: str
-            a path for the output .align file
     """
-
+    # POST MUSCLE #
     url = 'https://www.ebi.ac.uk/Tools/services/rest/muscle/run'
-    files = {'sequence': open(edits_filedir / 'sequences.fasta', 'rb')}
+    files = {'sequence': open(edits_filedir / seqs_filename, 'rb')}
     data = {
-        'email': email, 
-        'title': title, 
-        'format': 'clw',
-        'tree': 'none',
+        'email': email, 'title': title, 'format': 'clw', 'tree': 'none',
     }
     response = requests.post(url, data=data, files=files)
-    assert response.status_code == 200, 'Error with MUSCLE query'
-    time.sleep(30)
-    ### instead of waiting 30 s, should wait 5 s then check until received
+    if response.status_code != 200: 
+        warnings.warn('Error with MUSCLE query')
+    time.sleep(wait_time)
 
-    job_id = response.text
-    job_url_code = job_id.split(' ')[-1]
+    # GET MUSCLE #
+    job_url_code = response.text.split(' ')[-1]
     print(f'Job ID: {job_url_code}')
-
     url = f'https://www.ebi.ac.uk/Tools/services/rest/muscle/result/{job_url_code}/aln-clustalw'
     response = requests.get(url)
 
-    outpath_name = edits_filedir / f"Human{input_human_gene}_Mouse{input_mouse_gene}.align"
-    with open(outpath_name, "wb") as f:
+    with open(edits_filedir / align_filename, "wb") as f:
         f.write(response.content)
+    return job_url_code
 
-    return job_url_code, outpath_name
+def parse_alignment(
+    edits_filedir, 
+    align_filename, alignconserv_filename, residuemap_filename, 
+): 
+    # PARSE .align FILE #
+    iAlign = open(edits_filedir / align_filename, "r")
+    i_lines = iAlign.readlines()[3:]
+    iAlign.close()
+    ind = i_lines[0].rfind(' ')
+
+    # EXTRACT ALIGNMENT RESIDUES, ASSIGNS CONSERVATION SCORES, ADD TO DATAFRAME #
+    # takes into account diff start positions and diff spacing
+    cleaned_ilines = [s for s in [s[ind+1:].strip('\n') for s in i_lines] if len(s) > 0]
+    human_align_res = list(''.join([s for i, s in enumerate(cleaned_ilines) if     i%3 == 0]))
+    mouse_align_res = list(''.join([s for i, s in enumerate(cleaned_ilines) if (i-1)%3 == 0]))
+    score           = list(''.join([s for i, s in enumerate(cleaned_ilines) if (i-2)%3 == 0]))
+
+    index  = [i+1 for i in range(len(human_align_res))]
+    dis, v = [cons_dict[s][0] for s in score], [cons_dict[s][1] for s in score]
+    colnames = ['alignment_pos', 'human_aligned_res', 'mouse_aligned_res', 'score', 'dis_score', 'v_score']
+    colvals  = [index, human_align_res, mouse_align_res, score, dis, v]
+
+    df_alignconserv = pd.DataFrame()
+    for name, col in zip(colnames, colvals): 
+        df_alignconserv[name] = col
+    df_alignconserv.to_csv(edits_filedir / alignconserv_filename, sep='\t', index=False)
+
+    # COUNT CONSERVATION POSITIONS, ADD TO DATAFRAME #
+    i, j = 0, 0
+    human_res_pos, mouse_res_pos = [], []
+    for s in human_align_res: 
+        if s != '-': 
+            i += 1
+        human_res_pos.append(i)
+    for s in mouse_align_res: 
+        if s != '-': 
+            j += 1
+        mouse_res_pos.append(j)
+
+    colnames = ['alignment_pos', 'human_res_pos', 'human_res', 'mouse_res_pos', 'mouse_res', 'conservation']
+    colvals  = [index, human_res_pos, human_align_res, mouse_res_pos, mouse_align_res, dis]
+    df_residuemap = pd.DataFrame()
+    for name, col in zip(colnames, colvals): 
+        df_residuemap[name] = col
+    df_residuemap = df_residuemap[df_residuemap['human_res'] != '-']
+    df_residuemap.to_csv(edits_filedir / residuemap_filename, sep='\t', index=False)
+
+    del cleaned_ilines, index, human_align_res, mouse_align_res, score, dis, v, human_res_pos, mouse_res_pos, colnames, colvals
+
+    return df_alignconserv, df_residuemap
