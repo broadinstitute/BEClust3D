ccc20c94517f6d3b110a2ca8b46b3f568da52268
Calvin XiaoYang Hu <calvinxyh@Calvins-MacBook-Pro.local>
2024-10-04 00:26:43 -0400

    error with capitalization mean_missense_LFC

--- beclust3d/calculate_lfc3d.py
+++ beclust3d/calculate_lfc3d.py
@@ -103,19 +103,19 @@ def helper(
     # there are no residues nearby for low radius
     if not isinstance(naa_pos_str, float): 
         naa_pos_list = naa_pos_str.split(';') # neighboring residue positions
-        taa_LFC = df_str_cons_edits.at[aa, 'mean_Missense_LFC'] # target LFC
+        taa_LFC = df_str_cons_edits.at[aa, 'mean_missense_LFC'] # target LFC
 
         if taa_LFC != '-':
             taa_naa_wBE_LFC = 1
             sum_taa_naa_LFC = float(taa_LFC)
 
         for naa_pos in naa_pos_list: 
-            naa_LFC = df_str_cons_edits.at[int(naa_pos)-1, 'mean_Missense_LFC']
+            naa_LFC = df_str_cons_edits.at[int(naa_pos)-1, 'mean_missense_LFC']
             if naa_LFC != '-': 
                 sum_taa_naa_LFC += float(naa_LFC)
                 taa_naa_wBE_LFC += 1
     else: 
-        taa_LFC = df_str_cons_edits.at[aa, 'mean_Missense_LFC'] # target LFC
+        taa_LFC = df_str_cons_edits.at[aa, 'mean_missense_LFC'] # target LFC
         if taa_LFC != '-':
             taa_naa_wBE_LFC = 1
             sum_taa_naa_LFC = float(taa_LFC)
