8b8329892b0e145387931aa89845f93e065e8ba8
Yoochan <yuchan.m@gmail.com>
2025-04-18 14:07:47 -0400

    Add clustering_union func for LFC, LFC3D and Union clustering

--- beclust3d/annotate_spatial_clusters.py
+++ beclust3d/annotate_spatial_clusters.py
@@ -17,6 +17,252 @@ from pathlib import Path
 import os
 import warnings
 
+def clustering_union(
+        df_struc, df_LFC_pvals, df_LFC3D_pvals,
+        workdir, input_gene, screen_name = '', 
+        score_type='LFC',
+        max_distances=20, pthr_cutoff=0.05,
+        clustering_kwargs = {"n_clusters": None, "metric": "euclidean", "linkage": "single", },
+        subplots_kwargs={'figsize':(10,7)},
+        meta_aggregation=False,
+): 
+    """
+    Description
+        Calculates number of clusters for a range of clustering radii
+
+    Params
+        df_struc: pandas dataframe, required
+            DataFrame containing the x y z coordinates of the same length
+        df_LFC_pvals: pandas dataframe
+            from previous step prioritize_by_sequence() or average_split_bin_metaaggregation()
+        df_LFC3D_pvals: pandas dataframe
+            from previous step average_split_bin_lfc3d() or average_split_bin_metaaggregation()
+        workdir: str, required
+            the working directory
+        input_gene: str, required
+            the name of the input human gene
+        screen_name: str, optional
+            the name of the input screen
+        score_type: str, optional
+            'LFC', 'LFC3D', 'union
+        max_distances: int, optional
+            the upper limit of the range to test for clustering threshold
+        pthr_cutoff: float, optional
+            the p-value threshold
+        clustering_kwargs: dict, optional
+            input params into AgglomerativeClustering
+        subplots_kwargs: dict, optional
+
+    Returns
+        None
+    """
+    p_to_z_dict= {0.05:1.64, 0.01: 2.32, 0.001:3.09}
+    directions = ['neg','pos']
+
+    edits_filedir = Path(workdir)
+    df_LFC_pvals = df_LFC_pvals.filter(regex=fr'unipos|unires|LFC')
+    df_hits_clust = df_LFC_pvals.copy()
+    df_hits_clust[["x_coord", "y_coord", "z_coord"]] = df_struc
+    df_hits_clust = df_hits_clust.replace('-',None)
+    
+    if meta_aggregation:
+        output_dir = os.path.join(edits_filedir,'metaaggregation',f'cluster_{score_type}')
+        lfc_neg_col = 'SUM_LFC_neg'
+        lfc_pos_col = 'SUM_LFC_pos'
+        lfc_z_neg_col = f'SUM_LFC_neg_{str(pthr_cutoff).replace(".","")}_z'
+        lfc_z_pos_col = f'SUM_LFC_pos_{str(pthr_cutoff).replace(".","")}_z'
+        
+        lfc3d_neg_col = 'SUM_LFC3D_neg'
+        lfc3d_pos_col = 'SUM_LFC3D_pos'
+        lfc3d_z_neg_col = f'SUM_LFC3D_neg_{str(pthr_cutoff).replace(".","")}_z'
+        lfc3d_z_pos_col = f'SUM_LFC3D_pos_{str(pthr_cutoff).replace(".","")}_z'
+        df_hits_clust[lfc_neg_col] = df_hits_clust[lfc_neg_col].astype(float)
+        df_hits_clust[lfc_pos_col] = df_hits_clust[lfc_pos_col].astype(float)
+        df_hits_clust[lfc_z_neg_col] = df_hits_clust[lfc_z_neg_col].astype(float)
+        df_hits_clust[lfc_z_pos_col] = df_hits_clust[lfc_z_pos_col].astype(float)
+        
+        df_LFC3D_pvals = df_LFC3D_pvals.replace('-', None)
+        df_LFC3D_pvals = df_LFC3D_pvals.filter(regex=fr'{screen_name}_.+_z$|{lfc3d_neg_col}$|{lfc3d_z_neg_col}$|{lfc3d_pos_col}$|{lfc3d_z_pos_col}$')
+        df_LFC3D_pvals = df_LFC3D_pvals.astype(float)
+                
+    else:
+        output_dir = os.path.join(edits_filedir,f'cluster_{score_type}')
+        lfc_original_col = 'mean_Missense_LFC'
+        lfc_z_original_col = f'{lfc_original_col}_Z'
+        lfc_neg_col = 'LFC_neg'
+        lfc_pos_col = 'LFC_pos'
+        lfc_z_neg_col = 'LFC_neg_z'
+        lfc_z_pos_col = 'LFC_pos_z'
+        
+        lfc3d_neg_col = f'{screen_name}_LFC3D_neg'
+        lfc3d_pos_col = f'{screen_name}_LFC3D_pos'
+        lfc3d_z_neg_col = f'{screen_name}_LFC3D_neg_z'
+        lfc3d_z_pos_col = f'{screen_name}_LFC3D_pos_z'        
+        
+        df_hits_clust[lfc_original_col] = df_hits_clust[lfc_original_col].astype(float)
+        df_hits_clust[lfc_z_original_col] = df_hits_clust[lfc_z_original_col].astype(float)
+        
+        df_hits_clust[lfc_neg_col] = df_hits_clust[df_hits_clust[lfc_original_col] < 0][lfc_original_col]
+        df_hits_clust[lfc_pos_col] = df_hits_clust[df_hits_clust[lfc_original_col] > 0][lfc_original_col]
+
+        df_hits_clust[lfc_z_neg_col] = df_hits_clust[df_hits_clust[lfc_original_col] < 0][lfc_z_original_col]
+        df_hits_clust[lfc_z_pos_col] = df_hits_clust[df_hits_clust[lfc_original_col] > 0][lfc_z_original_col]
+        df_LFC3D_pvals = df_LFC3D_pvals.replace('-', None)
+        df_LFC3D_pvals = df_LFC3D_pvals.filter(regex=fr'{screen_name}_.+_z$|{lfc3d_neg_col}$|{lfc3d_pos_col}$')
+        df_LFC3D_pvals = df_LFC3D_pvals.astype(float)
+        
+
+    if not os.path.exists(edits_filedir):
+        os.mkdir(edits_filedir)
+        
+    if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+    
+
+    df_hits_clust = pd.concat([df_hits_clust, df_LFC3D_pvals],axis=1) # MERGE LFC and LFC3D COLUMNS
+    
+    # CLUSTERING #
+    arr_d_thr = [float(i+1) for i in range(2,max_distances)] # CLUSTERING DISTANCE HYPERPARAM
+    column_lists = [[] for _ in directions]
+    columns_dict = dict(zip(directions, column_lists)) # FOR EVERY DATA COLUMN TO CLUSTER
+    
+    if score_type == 'LFC':
+        for name, arr in columns_dict.items(): 
+            if name == 'neg':
+                df_pvals_temp = df_hits_clust[(df_hits_clust[lfc_neg_col] < 0)].copy()
+                df_pvals_temp = df_pvals_temp[df_pvals_temp[lfc_z_neg_col].notna()].reset_index(drop=True)
+                df_pvals_temp = df_pvals_temp.loc[(df_pvals_temp[lfc_z_neg_col] < -p_to_z_dict[pthr_cutoff]), ].reset_index(drop=True)
+                
+            elif name == 'pos':
+                df_pvals_temp = df_hits_clust[(df_hits_clust[lfc_pos_col] > 0)].copy()
+                df_pvals_temp = df_pvals_temp[df_pvals_temp[lfc_z_pos_col].notna()].reset_index(drop=True)
+                df_pvals_temp = df_pvals_temp.loc[(df_pvals_temp[lfc_z_pos_col] > p_to_z_dict[pthr_cutoff]), ].reset_index(drop=True)
+            else:
+                AssertionError, f'No columns for {directions} !'
+
+            # EXTRACT ROWS ABOVE CUTOFF #
+            dict_hits = {}
+
+            # REMOVE ROWS WITHOUT POSITION INFO FOR PDBs #
+            df_pvals_temp = df_pvals_temp[~df_pvals_temp[['x_coord', 'y_coord', 'z_coord']].isin(['-']).any(axis=1)]
+            dict_hits['unipos'] = list(df_pvals_temp['unipos'])
+
+            # EXTRACT X Y Z OF HITS ABOVE CUTOFF #
+            np_META_hits_coord = np.array(df_pvals_temp[['x_coord', 'y_coord', 'z_coord']].copy())
+            if np_META_hits_coord.shape[0] < 2: # NO DATA TO CLUSTER ON #
+                warnings.warn(f"Not enough data to perform agglomerative clustering")
+                _list = [0] * max_distances            
+                arr.extend(_list)
+                continue
+
+            # FOR RANGE OF RADIUS, RUN CLUSTERING #
+            for dist in arr_d_thr: 
+                func_clustering = AgglomerativeClustering(**clustering_kwargs, distance_threshold=dist)
+                clus_lbl = func_clustering.fit(np_META_hits_coord).labels_
+
+                n_c_output = int(max(clus_lbl)+1)
+                # print(f'Number of clusters of {name} hits: @ d = {dist} {n_c_output}')
+                arr.append(n_c_output)
+
+                dict_hits[f"{name}_Clust_{str(int(dist))}A"] = clus_lbl
+                plot_dendrogram(func_clustering, df_pvals_temp, edits_filedir, 
+                        name, input_gene, screen_name, score_type, dist, meta_aggregation, subplots_kwargs)
+
+            df_hits_clust = df_hits_clust.merge(pd.DataFrame(dict_hits), how='left', on=['unipos'])
+            
+    elif score_type == 'LFC3D':
+        for name, arr in columns_dict.items(): 
+            if name == 'neg':
+                df_pvals_temp = df_hits_clust[(df_hits_clust[lfc3d_neg_col] < 0)].copy()
+                df_pvals_temp = df_pvals_temp[df_pvals_temp[lfc3d_z_neg_col].notna()].reset_index(drop=True)
+                df_pvals_temp = df_pvals_temp.loc[(df_pvals_temp[lfc3d_z_neg_col] < -p_to_z_dict[pthr_cutoff]), ].reset_index(drop=True)
+                
+            elif name == 'pos':
+                df_pvals_temp = df_hits_clust[(df_hits_clust[lfc3d_pos_col] > 0)].copy()
+                df_pvals_temp = df_pvals_temp[df_pvals_temp[lfc3d_z_pos_col].notna()].reset_index(drop=True)
+                df_pvals_temp = df_pvals_temp.loc[(df_pvals_temp[lfc3d_z_pos_col] > p_to_z_dict[pthr_cutoff]), ].reset_index(drop=True)
+            else:
+                AssertionError, f'No columns for {directions} !'
+
+            # EXTRACT ROWS ABOVE CUTOFF #
+            dict_hits = {}
+
+            # REMOVE ROWS WITHOUT POSITION INFO FOR PDBs #
+            df_pvals_temp = df_pvals_temp[~df_pvals_temp[['x_coord', 'y_coord', 'z_coord']].isin(['-']).any(axis=1)]
+            dict_hits['unipos'] = list(df_pvals_temp['unipos'])
+
+            # EXTRACT X Y Z OF HITS ABOVE CUTOFF #
+            np_META_hits_coord = np.array(df_pvals_temp[['x_coord', 'y_coord', 'z_coord']].copy())
+            if np_META_hits_coord.shape[0] < 2: # NO DATA TO CLUSTER ON #
+                warnings.warn(f"Not enough data to perform agglomerative clustering")
+                _list = [0] * max_distances            
+                arr.extend(_list)
+                continue
+
+            # FOR RANGE OF RADIUS, RUN CLUSTERING #
+            for dist in arr_d_thr: 
+                func_clustering = AgglomerativeClustering(**clustering_kwargs, distance_threshold=dist)
+                clus_lbl = func_clustering.fit(np_META_hits_coord).labels_
+
+                n_c_output = int(max(clus_lbl)+1)
+                # print(f'Number of clusters of {name} hits: @ d = {dist} {n_c_output}')
+                arr.append(n_c_output)
+
+                dict_hits[f"{name}_Clust_{str(int(dist))}A"] = clus_lbl
+                plot_dendrogram(func_clustering, df_pvals_temp, edits_filedir, 
+                        name, input_gene, screen_name, score_type, dist, meta_aggregation, subplots_kwargs)
+
+            df_hits_clust = df_hits_clust.merge(pd.DataFrame(dict_hits), how='left', on=['unipos'])        
+    else:
+        # UNION
+        for name, arr in columns_dict.items():
+            df_pvals_temp = df_hits_clust.copy()
+            if 'neg' in name:
+                df_pvals_temp = df_pvals_temp[((df_pvals_temp[lfc_z_neg_col] < -p_to_z_dict[pthr_cutoff]) & (df_pvals_temp[lfc_neg_col] < 0)) | ((df_pvals_temp[lfc3d_z_neg_col] < -p_to_z_dict[pthr_cutoff]) & (df_pvals_temp[lfc3d_neg_col] < 0))].copy()
+            else:
+                df_pvals_temp = df_pvals_temp[((df_pvals_temp[lfc_z_pos_col] > p_to_z_dict[pthr_cutoff]) & (df_pvals_temp[lfc_pos_col] > 0)) | ((df_pvals_temp[lfc3d_z_pos_col] > p_to_z_dict[pthr_cutoff]) & (df_pvals_temp[lfc3d_pos_col] > 0))].copy()
+                
+            # EXTRACT ROWS ABOVE CUTOFF #
+            dict_hits = {}
+
+            # REMOVE ROWS WITHOUT POSITION INFO FOR PDBs #
+            df_pvals_temp = df_pvals_temp[~df_pvals_temp[['x_coord', 'y_coord', 'z_coord']].isin(['-']).any(axis=1)]
+            dict_hits['unipos'] = list(df_pvals_temp['unipos'])
+
+            # EXTRACT X Y Z OF HITS ABOVE CUTOFF #
+            np_META_hits_coord = np.array(df_pvals_temp[['x_coord', 'y_coord', 'z_coord']].copy())
+            if np_META_hits_coord.shape[0] < 2: # NO DATA TO CLUSTER ON #
+                warnings.warn(f"Not enough data to perform agglomerative clustering")
+                _list = [0] * max_distances
+                arr.extend(_list)
+                continue
+
+            # FOR RANGE OF RADIUS, RUN CLUSTERING #
+            for dist in arr_d_thr: 
+                func_clustering = AgglomerativeClustering(**clustering_kwargs, distance_threshold=dist)
+                clus_lbl = func_clustering.fit(np_META_hits_coord).labels_
+
+                n_c_output = int(max(clus_lbl)+1)
+                # print(f'Number of clusters of {name} hits: @ d = {dist} {n_c_output}')
+                arr.append(n_c_output)
+
+                dict_hits[f"{name}_Union_Clust_{str(int(dist))}A"] = clus_lbl
+                plot_dendrogram(func_clustering, df_pvals_temp, edits_filedir, 
+                        name, input_gene, screen_name, score_type, dist, meta_aggregation, subplots_kwargs)
+            df_hits_clust = df_hits_clust.merge(pd.DataFrame(dict_hits), how='left', on=['unipos'])            
+        
+    df_hits_clust.fillna('-')
+    hits_filename = edits_filedir / f"cluster_{score_type}/{input_gene}_{screen_name}_Aggr_Hits.tsv"
+    df_hits_clust.to_csv(hits_filename, sep='\t', index=False)
+ 
+    # PLOT #
+    yvals = list(columns_dict.values())
+
+    plot_cluster_distance(arr_d_thr, yvals, directions, edits_filedir, 
+                          input_gene, screen_name, score_type, subplots_kwargs, str(pthr_cutoff))
+
+    return df_hits_clust, arr_d_thr, yvals
+
 def clustering(
         df_struc, df_pvals, 
         workdir, input_gene, screen_name = 'Meta', 
